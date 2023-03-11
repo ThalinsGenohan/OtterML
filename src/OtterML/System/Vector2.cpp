9b2167652489243848449f38a18fb82407ddf5c9
Thalins Genohan <32170993+ThalinsGenohan@users.noreply.github.com>
2023-03-11 01:04:49 -0500

    Update Vector2.cpp

---
+++ src/OtterML/System/Vector2.cpp
@@ -0,0 +1,17 @@
+#include <OtterML/System/Vector2.hpp>
+
+#include <cmath>
+
+namespace oter
+{
+template <typename T> float Vector2<T>::GetLength()
+{
+	return std::sqrt(this->X * this->X + this->Y * this->Y);
+}
+
+template <typename T>
+Vector2<T> Vector2<T>::GetNormalized()
+{
+	return *this / this->GetLength();
+}
+}
